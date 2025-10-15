60095b54065f4986c4ac2ad4b439966df12b0e33
André Montanari <35246438+alphamontanari@users.noreply.github.com>
2025-10-15 12:01:46 -0300

    v.1.3.8.3
    add linha 16A

---
+++ assets/linhas/linha16A.js
@@ -0,0 +1,78 @@
+const Linha16A = {
+    "id": "16A",
+    "cod": "Linha16A",
+    "nome": "PARQUE ITAPÊ → VILA SERAFIM → VILA SOTTEMO",
+    "partida": "PARQUE ITAPÊ",
+    "chegada": "VILA SOTTEMO",
+    "horarios": {
+        "dia_de_semana": {
+            "06:55": {
+                "atendimento": {
+                    "PARQUE ITAPÊ": "06:55",
+                    "VILA MAZZEI": "07:00",
+                    "RESIDENCIAL CRISTIVÃO COLOMBO (PACAEMBU 2)": "07:05",
+                    "AV. WALDOMIRO DE CARVALHO": "07:10",
+                    "VILA RIO BRANCO": "07:15",
+                    "MERCADO MUNICIPAL": "07:25",                 
+                }, servico: 'BAIRRO → CENTRO', trajeto: 'diferenciado'
+            },
+            "11:35": {
+                "atendimento": {
+                    "PARQUE ITAPÊ": "11:35",
+                    "VILA MAZZEI": "11:40",
+                    "RESIDENCIAL CRISTIVÃO COLOMBO (PACAEMBU 2)": "11:45",
+                    "AV. WALDOMIRO DE CARVALHO": "11:50",
+                    "VILA RIO BRANCO": "11:55",
+                    "MERCADO MUNICIPAL": "12:05",
+                    "RODOVIÁRIA": "12:20",
+                    "VILA APARECIDA": "12:22",
+                    "RECINTO DE EXPOSIÇÕES ACÁCIO DE MORAES TERRA (VIA MARGINAL)": "12:25",
+                    "VILA NOVA ITAPETININGA (VIA MARGINAL)": "12:27",
+                    "VILA SERAFIM": "12:30",
+                    "VILA SÃO JOSÉ": "12:33",
+                    "VILA BARTH I": "12:35",
+                    "PASSARELA DOMENICO ALCIATI (JD. BELA VISTA)": "12:40",
+                    "VILA SOTTEMO": "12:50"
+                }, servico: 'BAIRRO → CENTRO → BAIRRO',
+            },
+            "14:20": {
+                "atendimento": {
+                    "PARQUE ITAPÊ": "14:20",
+                    "VILA MAZZEI": "14:25",
+                    "RESIDENCIAL CRISTIVÃO COLOMBO (PACAEMBU 2)": "14:30",
+                    "AV. WALDOMIRO DE CARVALHO": "14:35",
+                    "VILA RIO BRANCO": "14:40",
+                    "MERCADO MUNICIPAL": "14:50",
+                    "RODOVIÁRIA": "15:05",
+                    "VILA APARECIDA": "15:15",
+                    "RECINTO DE EXPOSIÇÕES ACÁCIO DE MORAES TERRA (VIA MARGINAL)": "15:18",
+                    "VILA NOVA ITAPETININGA (VIA MARGINAL)": "15:23",
+                    "VILA SERAFIM": "15:28",
+                    "VILA SÃO JOSÉ": "15:33",
+                    "VILA BARTH I": "15:35",
+                    "PASSARELA DOMENICO ALCIATI (JD. BELA VISTA)": "15:40",
+                    "VILA SOTTEMO": "15:50"
+                }
+            },
+            "17:35": {
+                "atendimento": {
+                    "PARQUE ITAPÊ": "17:35",
+                    "VILA MAZZEI": "17:40",
+                    "RESIDENCIAL CRISTIVÃO COLOMBO (PACAEMBU 2)": "17:45",
+                    "AV. WALDOMIRO DE CARVALHO": "17:50",
+                    "VILA RIO BRANCO": "17:55",
+                    "MERCADO MUNICIPAL": "18:05",
+                    "RODOVIÁRIA": "18:20",
+                    "VILA APARECIDA": "18:30",
+                    "RECINTO DE EXPOSIÇÕES ACÁCIO DE MORAES TERRA (VIA MARGINAL)": "18:33",
+                    "VILA NOVA ITAPETININGA (VIA MARGINAL)": "18:38",
+                    "VILA SERAFIM": "18:43",
+                    "VILA SÃO JOSÉ": "18:48",
+                    "VILA BARTH I": "18:50",
+                    "PASSARELA DOMENICO ALCIATI (JD. BELA VISTA)": "18:55",
+                    "VILA SOTTEMO": "19:05"
+                }
+            }
+        }
+    }
+}
