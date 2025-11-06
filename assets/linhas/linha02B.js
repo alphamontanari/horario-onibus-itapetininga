339f9fe3e4ddccd11e7a3a984d9ac87ca2a19d23
André Montanari <35246438+alphamontanari@users.noreply.github.com>
2025-11-06 17:22:30 -0300

    1.3.8.15
    Atualização horários do sábdo da linhas 2B

--- assets/linhas/linha02B.js
+++ assets/linhas/linha02B.js
@@ -187,17 +187,17 @@ const Linha02B = {
         }, trajeto: 'diferenciado'
       }
     },
-    "sabado": {/*
-      "06:00": {
+    "sabado": {
+      "05:50": {
         "atendimento": {
-          "BAIRRO TABOÃOZINHO": "06:00",
-          "JD. FOGAÇA": "06:06",
-          "RODOVIÁRIA": "06:16",
-          "MERCADO MUNICIPAL": "06:36",
-          "AV. CINCO DE NOVEMBRO": "06:49",
-          "VILA CAROLINA": "06:52",
-          "RESIDENCIAL CAMBUÍ": "06:57",
-          "JD. BELA VISTA": "07:00"
+          "BAIRRO TABOÃOZINHO": "05:50",
+          "JD. FOGAÇA": "05:56",
+          "RODOVIÁRIA": "06:06",
+          "MERCADO MUNICIPAL": "06:30",
+          "AV. CINCO DE NOVEMBRO": "06:46",
+          "VILA CAROLINA": "06:49",
+          "RESIDENCIAL CAMBUÍ": "06:54",
+          "JD. BELA VISTA": "06:57"
         }
       },
       "07:00": {
@@ -205,270 +205,119 @@ const Linha02B = {
           "BAIRRO TABOÃOZINHO": "07:00",
           "JD. FOGAÇA": "07:06",
           "RODOVIÁRIA": "07:16",
-          "MERCADO MUNICIPAL": "07:36",
-          "AV. CINCO DE NOVEMBRO": "07:49",
-          "VILA CAROLINA": "07:52",
-          "RESIDENCIAL CAMBUÍ": "07:57",
-          "JD. BELA VISTA": "08:00"
-        }
-      },
-      "08:00": {
-        "atendimento": {
-          "BAIRRO TABOÃOZINHO": "08:00",
-          "JD. FOGAÇA": "08:06",
-          "RODOVIÁRIA": "08:16",
-          "MERCADO MUNICIPAL": "08:36",
-          "AV. CINCO DE NOVEMBRO": "08:49",
-          "VILA CAROLINA": "08:52",
-          "RESIDENCIAL CAMBUÍ": "08:57",
-          "JD. BELA VISTA": "09:00"
-        }
-      },
-      "09:00": {
-        "atendimento": {
-          "BAIRRO TABOÃOZINHO": "09:00",
-          "JD. FOGAÇA": "09:06",
-          "RODOVIÁRIA": "09:16",
-          "MERCADO MUNICIPAL": "09:36",
-          "AV. CINCO DE NOVEMBRO": "09:49",
-          "VILA CAROLINA": "09:52",
-          "RESIDENCIAL CAMBUÍ": "09:57",
-          "JD. BELA VISTA": "10:00"
-        }
-      },
-      "10:00": {
-        "atendimento": {
-          "BAIRRO TABOÃOZINHO": "10:00",
-          "JD. FOGAÇA": "10:06",
-          "RODOVIÁRIA": "10:16",
-          "MERCADO MUNICIPAL": "10:36",
-          "AV. CINCO DE NOVEMBRO": "10:49",
-          "VILA CAROLINA": "10:52",
-          "RESIDENCIAL CAMBUÍ": "10:57",
-          "JD. BELA VISTA": "11:00"
-        }
-      },
-      "11:00": {
-        "atendimento": {
-          "BAIRRO TABOÃOZINHO": "11:00",
-          "JD. FOGAÇA": "11:06",
-          "RODOVIÁRIA": "11:16",
-          "MERCADO MUNICIPAL": "11:36",
-          "AV. CINCO DE NOVEMBRO": "11:49",
-          "VILA CAROLINA": "11:52",
-          "RESIDENCIAL CAMBUÍ": "11:57",
-          "JD. BELA VISTA": "12:00"
-        }
-      },
-      "12:00": {
-        "atendimento": {
-          "BAIRRO TABOÃOZINHO": "12:00",
-          "JD. FOGAÇA": "12:06",
-          "RODOVIÁRIA": "12:16",
-          "MERCADO MUNICIPAL": "12:36",
-          "AV. CINCO DE NOVEMBRO": "12:49",
-          "VILA CAROLINA": "12:52",
-          "RESIDENCIAL CAMBUÍ": "12:57",
-          "JD. BELA VISTA": "13:00"
-        }
-      },
-      "13:00": {
-        "atendimento": {
-          "BAIRRO TABOÃOZINHO": "13:00",
-          "JD. FOGAÇA": "13:06",
-          "RODOVIÁRIA": "13:16",
-          "MERCADO MUNICIPAL": "13:36",
-          "AV. CINCO DE NOVEMBRO": "13:49",
-          "VILA CAROLINA": "13:52",
-          "RESIDENCIAL CAMBUÍ": "13:57",
-          "JD. BELA VISTA": "14:00"
-        }
-      },
-      "14:00": {
-        "atendimento": {
-          "BAIRRO TABOÃOZINHO": "14:00",
-          "JD. FOGAÇA": "14:06",
-          "RODOVIÁRIA": "14:16"
-        }, trajeto : 'diferenciado'
-      },
-      "16:00": {
-        "atendimento": {
-          "BAIRRO TABOÃOZINHO": "16:00",
-          "JD. FOGAÇA": "16:06",
-          "RODOVIÁRIA": "16:16",
-          "MERCADO MUNICIPAL": "16:36",
-          "AV. CINCO DE NOVEMBRO": "16:49",
-          "VILA CAROLINA": "16:52",
-          "RESIDENCIAL CAMBUÍ": "16:57",
-          "JD. BELA VISTA": "17:00"
-        }
-      },
-      "17:00": {
-        "atendimento": {
-          "BAIRRO TABOÃOZINHO": "17:00",
-          "JD. FOGAÇA": "17:06",
-          "RODOVIÁRIA": "17:16",
-          "MERCADO MUNICIPAL": "17:36",
-          "AV. CINCO DE NOVEMBRO": "17:49",
-          "VILA CAROLINA": "17:52",
-          "RESIDENCIAL CAMBUÍ": "17:57",
-          "JD. BELA VISTA": "18:00"
-        }
-      },
-      "18:00": {
-        "atendimento": {
-          "BAIRRO TABOÃOZINHO": "18:00",
-          "JD. FOGAÇA": "18:06",
-          "RODOVIÁRIA": "18:16",
-          "MERCADO MUNICIPAL": "18:36",
-          "AV. CINCO DE NOVEMBRO": "18:49",
-          "VILA CAROLINA": "18:52",
-          "RESIDENCIAL CAMBUÍ": "18:57",
-          "JD. BELA VISTA": "19:00"
-        }
-      },
-      "19:00": {
-        "atendimento": {
-          "BAIRRO TABOÃOZINHO": "19:00",
-          "JD. FOGAÇA": "19:06",
-          "RODOVIÁRIA": "19:16",
-          "MERCADO MUNICIPAL": "19:36",
-          "AV. CINCO DE NOVEMBRO": "19:49",
-          "VILA CAROLINA": "19:52",
-          "RESIDENCIAL CAMBUÍ": "19:57",
-          "JD. BELA VISTA": "20:00"
-        }
-      }*/
-      "05:50": {
-        "atendimento": {
-          "BAIRRO TABOÃOZINHO": "05:50",
-          "JD. FOGAÇA": "05:56",
-          "RODOVIÁRIA": "06:08",
-          "MERCADO MUNICIPAL": "06:32",
-          "AV. CINCO DE NOVEMBRO": "06:48",
-          "VILA CAROLINA": "06:51",
-          "RESIDENCIAL CAMBUÍ": "06:56",
-          "JD. BELA VISTA": "06:59"
-        }
-      },
-      "07:00": {
-        "atendimento": {
-          "BAIRRO TABOÃOZINHO": "07:00",
-          "JD. FOGAÇA": "07:06",
-          "RODOVIÁRIA": "07:18",
-          "MERCADO MUNICIPAL": "07:42",
-          "AV. CINCO DE NOVEMBRO": "07:57",
-          "VILA CAROLINA": "08:00",
-          "RESIDENCIAL CAMBUÍ": "08:05",
-          "JD. BELA VISTA": "08:08"
+          "MERCADO MUNICIPAL": "07:26",
+          "AV. CINCO DE NOVEMBRO": "07:42",
+          "VILA CAROLINA": "07:45",
+          "RESIDENCIAL CAMBUÍ": "07:50",
+          "JD. BELA VISTA": "07:58"
         }
       },
       "08:10": {
         "atendimento": {
           "BAIRRO TABOÃOZINHO": "08:10",
           "JD. FOGAÇA": "08:16",
-          "RODOVIÁRIA": "08:28",
-          "MERCADO MUNICIPAL": "08:52",
-          "AV. CINCO DE NOVEMBRO": "09:07",
-          "VILA CAROLINA": "09:10",
-          "RESIDENCIAL CAMBUÍ": "09:15",
-          "JD. BELA VISTA": "09:18"
+          "RODOVIÁRIA": "08:26",
+          "MERCADO MUNICIPAL": "08:36",
+          "AV. CINCO DE NOVEMBRO": "08:52",
+          "VILA CAROLINA": "05:55",
+          "RESIDENCIAL CAMBUÍ": "09:00",
+          "JD. BELA VISTA": "09:03"
         }
       },
       "09:25": {
         "atendimento": {
           "BAIRRO TABOÃOZINHO": "09:25",
           "JD. FOGAÇA": "09:31",
-          "RODOVIÁRIA": "09:43",
-          "MERCADO MUNICIPAL": "10:07",
-          "AV. CINCO DE NOVEMBRO": "10:22",
-          "VILA CAROLINA": "10:25",
-          "RESIDENCIAL CAMBUÍ": "10:30",
-          "JD. BELA VISTA": "10:33"
+          "RODOVIÁRIA": "09:41",
+          "MERCADO MUNICIPAL": "09:51",
+          "AV. CINCO DE NOVEMBRO": "10:07",
+          "VILA CAROLINA": "10:10",
+          "RESIDENCIAL CAMBUÍ": "10:15",
+          "JD. BELA VISTA": "10:18"
         }
       },
       "10:40": {
         "atendimento": {
           "BAIRRO TABOÃOZINHO": "10:40",
           "JD. FOGAÇA": "10:46",
-          "RODOVIÁRIA": "10:58",
-          "MERCADO MUNICIPAL": "11:22",
-          "AV. CINCO DE NOVEMBRO": "11:37",
-          "VILA CAROLINA": "11:40",
-          "RESIDENCIAL CAMBUÍ": "11:45",
-          "JD. BELA VISTA": "11:48"
+          "RODOVIÁRIA": "10:56",
+          "MERCADO MUNICIPAL": "11:06",
+          "AV. CINCO DE NOVEMBRO": "11:22",
+          "VILA CAROLINA": "11:25",
+          "RESIDENCIAL CAMBUÍ": "11:30",
+          "JD. BELA VISTA": "11:33"
         }
       },
       "11:55": {
         "atendimento": {
           "BAIRRO TABOÃOZINHO": "11:55",
           "JD. FOGAÇA": "12:01",
-          "RODOVIÁRIA": "12:13",
-          "MERCADO MUNICIPAL": "12:37",
-          "AV. CINCO DE NOVEMBRO": "12:52",
-          "VILA CAROLINA": "12:55",
-          "RESIDENCIAL CAMBUÍ": "13:00",
-          "JD. BELA VISTA": "13:03"
+          "RODOVIÁRIA": "12:11",
+          "MERCADO MUNICIPAL": "12:21",
+          "AV. CINCO DE NOVEMBRO": "12:37",
+          "VILA CAROLINA": "12:40",
+          "RESIDENCIAL CAMBUÍ": "12:45",
+          "JD. BELA VISTA": "12:48"
         }
       },
       "13:10": {
         "atendimento": {
           "BAIRRO TABOÃOZINHO": "13:10",
           "JD. FOGAÇA": "13:16",
-          "RODOVIÁRIA": "13:28",
-          "MERCADO MUNICIPAL": "13:52",
-          "AV. CINCO DE NOVEMBRO": "14:07",
-          "VILA CAROLINA": "14:10",
-          "RESIDENCIAL CAMBUÍ": "14:15",
-          "JD. BELA VISTA": "14:18"
+          "RODOVIÁRIA": "13:26",
+          "MERCADO MUNICIPAL": "13:36",
+          "AV. CINCO DE NOVEMBRO": "13:52",
+          "VILA CAROLINA": "13:55",
+          "RESIDENCIAL CAMBUÍ": "14:00",
+          "JD. BELA VISTA": "14:03"
         }
       },
       "14:25": {
         "atendimento": {
           "BAIRRO TABOÃOZINHO": "14:25",
           "JD. FOGAÇA": "14:31",
-          "RODOVIÁRIA": "14:43",
-          "MERCADO MUNICIPAL": "15:07",
-          "AV. CINCO DE NOVEMBRO": "15:22",
-          "VILA CAROLINA": "15:25",
-          "RESIDENCIAL CAMBUÍ": "15:30",
-          "JD. BELA VISTA": "15:33"
+          "RODOVIÁRIA": "14:41",
+          "MERCADO MUNICIPAL": "14:51",
+          "AV. CINCO DE NOVEMBRO": "15:07",
+          "VILA CAROLINA": "15:10",
+          "RESIDENCIAL CAMBUÍ": "15:15",
+          "JD. BELA VISTA": "15:18"
         }
       },
       "15:40": {
         "atendimento": {
           "BAIRRO TABOÃOZINHO": "15:40",
           "JD. FOGAÇA": "15:46",
-          "RODOVIÁRIA": "15:58",
-          "MERCADO MUNICIPAL": "16:22",
-          "AV. CINCO DE NOVEMBRO": "16:37",
-          "VILA CAROLINA": "16:40",
-          "RESIDENCIAL CAMBUÍ": "16:45",
-          "JD. BELA VISTA": "16:48"
+          "RODOVIÁRIA": "15:56",
+          "MERCADO MUNICIPAL": "16:06",
+          "AV. CINCO DE NOVEMBRO": "16:22",
+          "VILA CAROLINA": "16:25",
+          "RESIDENCIAL CAMBUÍ": "16:30",
+          "JD. BELA VISTA": "16:33"
         }
       },
       "16:55": {
         "atendimento": {
           "BAIRRO TABOÃOZINHO": "16:55",
           "JD. FOGAÇA": "17:01",
-          "RODOVIÁRIA": "17:13",
-          "MERCADO MUNICIPAL": "17:37",
-          "AV. CINCO DE NOVEMBRO": "17:52",
-          "VILA CAROLINA": "17:55",
-          "RESIDENCIAL CAMBUÍ": "18:00",
-          "JD. BELA VISTA": "18:03"
+          "RODOVIÁRIA": "17:11",
+          "MERCADO MUNICIPAL": "17:21",
+          "AV. CINCO DE NOVEMBRO": "17:37",
+          "VILA CAROLINA": "17:40",
+          "RESIDENCIAL CAMBUÍ": "14:45",
+          "JD. BELA VISTA": "17:48"
         }
       },
       "18:10": {
         "atendimento": {
           "BAIRRO TABOÃOZINHO": "18:10",
           "JD. FOGAÇA": "18:16",
-          "RODOVIÁRIA": "18:28",
-          "MERCADO MUNICIPAL": "18:52",
-          "AV. CINCO DE NOVEMBRO": "19:07",
-          "VILA CAROLINA": "19:10",
-          "RESIDENCIAL CAMBUÍ": "19:15",
-          "JD. BELA VISTA": "19:18"
+          "RODOVIÁRIA": "18:26",
+          "MERCADO MUNICIPAL": "18:36",
+          "AV. CINCO DE NOVEMBRO": "18:52",
+          "VILA CAROLINA": "18:55",
+          "RESIDENCIAL CAMBUÍ": "19:00",
+          "JD. BELA VISTA": "19:03"
         }
       },
       "19:10": {
