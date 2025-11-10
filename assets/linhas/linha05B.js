c5ad2133ad574a0e5d843929c64eec7c11d1d9eb
André Montanari <35246438+alphamontanari@users.noreply.github.com>
2025-11-10 12:17:33 -0300

    1.3.8.25
    alterações nas linhas 05B e 06B horários de passagem Mercado Municipal

--- assets/linhas/linha05B.js
+++ assets/linhas/linha05B.js
@@ -11,11 +11,11 @@ const Linha05B = {
                     "PORTAL DOS PINHEIROS": "06:00",
                     "VILA PRADO": "06:05",
                     "VILA PAULO AYRES": "06:08",
-                    "VILA ARRUDA": "06:15",
-                    "RODOVIÁRIA": "06:25",
-                    "MERCADO MUNICIPAL": "06:35",
-                    "AV. CINCO DE NOVEMBRO": "06:45",
-                    "NASTRI II (BANCÁRIOS)": "06:50"
+                    "VILA ARRUDA": "06:09",
+                    "RODOVIÁRIA": "06:15",
+                    "MERCADO MUNICIPAL": "06:25",
+                    "AV. CINCO DE NOVEMBRO": "06:35",
+                    "NASTRI II (BANCÁRIOS)": "06:45"
                 }
             },
             "07:04": {
@@ -23,11 +23,11 @@ const Linha05B = {
                     "PORTAL DOS PINHEIROS": "07:04",
                     "VILA PRADO": "07:09",
                     "VILA PAULO AYRES": "07:12",
-                    "VILA ARRUDA": "07:19",
-                    "RODOVIÁRIA": "07:29",
-                    "MERCADO MUNICIPAL": "07:39",
-                    "AV. CINCO DE NOVEMBRO": "07:49",
-                    "NASTRI II (BANCÁRIOS)": "07:54"
+                    "VILA ARRUDA": "07:13",
+                    "RODOVIÁRIA": "07:19",
+                    "MERCADO MUNICIPAL": "07:29",
+                    "AV. CINCO DE NOVEMBRO": "07:39",
+                    "NASTRI II (BANCÁRIOS)": "07:50"
                 }
             },
             "08:00": {
@@ -35,9 +35,9 @@ const Linha05B = {
                     "PORTAL DOS PINHEIROS": "08:00",
                     "VILA PRADO": "08:05",
                     "VILA PAULO AYRES": "08:08",
-                    "VILA ARRUDA": "08:15",
-                    "RODOVIÁRIA": "08:25",
-                    "MERCADO MUNICIPAL": "08:35",
+                    "VILA ARRUDA": "08:09",
+                    "RODOVIÁRIA": "08:15",
+                    "MERCADO MUNICIPAL": "08:25",
                     "AV. CINCO DE NOVEMBRO": "08:45",
                     "NASTRI II (BANCÁRIOS)": "08:50"
                 }
@@ -47,11 +47,11 @@ const Linha05B = {
                     "PORTAL DOS PINHEIROS": "12:00",
                     "VILA PRADO": "12:05",
                     "VILA PAULO AYRES": "12:08",
-                    "VILA ARRUDA": "12:15",
-                    "RODOVIÁRIA": "12:25",
-                    "MERCADO MUNICIPAL": "12:35",
-                    "AV. CINCO DE NOVEMBRO": "12:45",
-                    "NASTRI II (BANCÁRIOS)": "12:50"
+                    "VILA ARRUDA": "12:09",
+                    "RODOVIÁRIA": "12:15",
+                    "MERCADO MUNICIPAL": "12:25",
+                    "AV. CINCO DE NOVEMBRO": "12:35",
+                    "NASTRI II (BANCÁRIOS)": "12:45"
                 }
             },
             "14:00": {
@@ -59,11 +59,11 @@ const Linha05B = {
                     "PORTAL DOS PINHEIROS": "14:00",
                     "VILA PRADO": "14:05",
                     "VILA PAULO AYRES": "14:08",
-                    "VILA ARRUDA": "14:15",
-                    "RODOVIÁRIA": "14:25",
-                    "MERCADO MUNICIPAL": "14:35",
-                    "AV. CINCO DE NOVEMBRO": "14:45",
-                    "NASTRI II (BANCÁRIOS)": "14:50"
+                    "VILA ARRUDA": "14:09",
+                    "RODOVIÁRIA": "14:15",
+                    "MERCADO MUNICIPAL": "14:25",
+                    "AV. CINCO DE NOVEMBRO": "14:35",
+                    "NASTRI II (BANCÁRIOS)": "14:45"
                 }
             },
             "16:00": {
@@ -71,11 +71,11 @@ const Linha05B = {
                     "PORTAL DOS PINHEIROS": "16:00",
                     "VILA PRADO": "16:05",
                     "VILA PAULO AYRES": "16:08",
-                    "VILA ARRUDA": "16:15",
-                    "RODOVIÁRIA": "16:25",
-                    "MERCADO MUNICIPAL": "16:35",
-                    "AV. CINCO DE NOVEMBRO": "16:45",
-                    "NASTRI II (BANCÁRIOS)": "16:50"
+                    "VILA ARRUDA": "16:09",
+                    "RODOVIÁRIA": "16:15",
+                    "MERCADO MUNICIPAL": "16:25",
+                    "AV. CINCO DE NOVEMBRO": "16:35",
+                    "NASTRI II (BANCÁRIOS)": "16:45"
                 }
             },
             "17:00": {
@@ -83,11 +83,11 @@ const Linha05B = {
                     "PORTAL DOS PINHEIROS": "17:00",
                     "VILA PRADO": "17:05",
                     "VILA PAULO AYRES": "17:08",
-                    "VILA ARRUDA": "17:15",
-                    "RODOVIÁRIA": "17:25",
-                    "MERCADO MUNICIPAL": "17:35",
-                    "AV. CINCO DE NOVEMBRO": "17:45",
-                    "NASTRI II (BANCÁRIOS)": "17:50"
+                    "VILA ARRUDA": "17:09",
+                    "RODOVIÁRIA": "17:15",
+                    "MERCADO MUNICIPAL": "17:25",
+                    "AV. CINCO DE NOVEMBRO": "17:35",
+                    "NASTRI II (BANCÁRIOS)": "17:45"
                 }
             },
             "18:00": {
@@ -95,10 +95,10 @@ const Linha05B = {
                     "PORTAL DOS PINHEIROS": "18:00",
                     "VILA PRADO": "18:05",
                     "VILA PAULO AYRES": "18:08",
-                    "VILA ARRUDA": "18:15",
-                    "RODOVIÁRIA": "18:25",
-                    "MERCADO MUNICIPAL": "18:35",
-                    "AV. CINCO DE NOVEMBRO": "18:45",
+                    "VILA ARRUDA": "18:09",
+                    "RODOVIÁRIA": "18:15",
+                    "MERCADO MUNICIPAL": "18:25",
+                    "AV. CINCO DE NOVEMBRO": "18:35",
                     "NASTRI II (BANCÁRIOS)": "18:50"
                 }
             },
@@ -107,8 +107,8 @@ const Linha05B = {
                     "PORTAL DOS PINHEIROS": "19:00",
                     "VILA PRADO": "19:05",
                     "VILA PAULO AYRES": "19:08",
-                    "VILA ARRUDA": "19:15",
-                    "RODOVIÁRIA": "19:25"
+                    "VILA ARRUDA": "19:09",
+                    "RODOVIÁRIA": "19:15"
                 }, trajeto: 'diferenciado'
             },
             "21:30": {
@@ -141,11 +141,11 @@ const Linha05B = {
                     "PORTAL DOS PINHEIROS": "06:00",
                     "VILA PRADO": "06:05",
                     "VILA PAULO AYRES": "06:08",
-                    "VILA ARRUDA": "06:15",
-                    "RODOVIÁRIA": "06:25",
-                    "MERCADO MUNICIPAL": "06:35",
-                    "AV. CINCO DE NOVEMBRO": "06:45",
-                    "NASTRI II (BANCÁRIOS)": "06:47"
+                    "VILA ARRUDA": "06:09",
+                    "RODOVIÁRIA": "06:15",
+                    "MERCADO MUNICIPAL": "06:25",
+                    "AV. CINCO DE NOVEMBRO": "06:35",
+                    "NASTRI II (BANCÁRIOS)": "06:40"
                 }
             },
             "08:00": {
@@ -153,11 +153,11 @@ const Linha05B = {
                     "PORTAL DOS PINHEIROS": "08:00",
                     "VILA PRADO": "08:05",
                     "VILA PAULO AYRES": "08:08",
-                    "VILA ARRUDA": "08:15",
-                    "RODOVIÁRIA": "08:25",
-                    "MERCADO MUNICIPAL": "08:35",
-                    "AV. CINCO DE NOVEMBRO": "08:45",
-                    "NASTRI II (BANCÁRIOS)": "08:47"
+                    "VILA ARRUDA": "08:09",
+                    "RODOVIÁRIA": "08:15",
+                    "MERCADO MUNICIPAL": "08:25",
+                    "AV. CINCO DE NOVEMBRO": "08:35",
+                    "NASTRI II (BANCÁRIOS)": "08:45"
                 }
             },
             "12:00": {
@@ -165,11 +165,11 @@ const Linha05B = {
                     "PORTAL DOS PINHEIROS": "12:00",
                     "VILA PRADO": "12:05",
                     "VILA PAULO AYRES": "12:08",
-                    "VILA ARRUDA": "12:15",
-                    "RODOVIÁRIA": "12:25",
-                    "MERCADO MUNICIPAL": "12:35",
-                    "AV. CINCO DE NOVEMBRO": "12:45",
-                    "NASTRI II (BANCÁRIOS)": "12:47"
+                    "VILA ARRUDA": "12:09",
+                    "RODOVIÁRIA": "12:15",
+                    "MERCADO MUNICIPAL": "12:25",
+                    "AV. CINCO DE NOVEMBRO": "12:35",
+                    "NASTRI II (BANCÁRIOS)": "12:45"
                 }
             },
             "17:00": {
@@ -177,11 +177,11 @@ const Linha05B = {
                     "PORTAL DOS PINHEIROS": "17:00",
                     "VILA PRADO": "17:05",
                     "VILA PAULO AYRES": "17:08",
-                    "VILA ARRUDA": "17:15",
-                    "RODOVIÁRIA": "17:25",
-                    "MERCADO MUNICIPAL": "17:35",
-                    "AV. CINCO DE NOVEMBRO": "17:45",
-                    "NASTRI II (BANCÁRIOS)": "17:47"
+                    "VILA ARRUDA": "17:09",
+                    "RODOVIÁRIA": "17:15",
+                    "MERCADO MUNICIPAL": "17:25",
+                    "AV. CINCO DE NOVEMBRO": "17:35",
+                    "NASTRI II (BANCÁRIOS)": "17:45"
                 }
             }
         },
@@ -191,11 +191,11 @@ const Linha05B = {
                     "PORTAL DOS PINHEIROS": "06:00",
                     "VILA PRADO": "06:05",
                     "VILA PAULO AYRES": "06:08",
-                    "VILA ARRUDA": "06:15",
-                    "RODOVIÁRIA": "06:25",
-                    "MERCADO MUNICIPAL": "06:35",
-                    "AV. CINCO DE NOVEMBRO": "06:45",
-                    "NASTRI II (BANCÁRIOS)": "06:47"
+                    "VILA ARRUDA": "06:09",
+                    "RODOVIÁRIA": "06:15",
+                    "MERCADO MUNICIPAL": "06:25",
+                    "AV. CINCO DE NOVEMBRO": "06:35",
+                    "NASTRI II (BANCÁRIOS)": "06:45"
                 }
             },
             "08:00": {
@@ -203,11 +203,11 @@ const Linha05B = {
                     "PORTAL DOS PINHEIROS": "08:00",
                     "VILA PRADO": "08:05",
                     "VILA PAULO AYRES": "08:08",
-                    "VILA ARRUDA": "08:15",
-                    "RODOVIÁRIA": "08:25",
-                    "MERCADO MUNICIPAL": "08:35",
-                    "AV. CINCO DE NOVEMBRO": "08:45",
-                    "NASTRI II (BANCÁRIOS)": "08:47"
+                    "VILA ARRUDA": "08:09",
+                    "RODOVIÁRIA": "08:15",
+                    "MERCADO MUNICIPAL": "08:25",
+                    "AV. CINCO DE NOVEMBRO": "08:35",
+                    "NASTRI II (BANCÁRIOS)": "08:45"
                 }
             },
             "12:00": {
@@ -215,11 +215,11 @@ const Linha05B = {
                     "PORTAL DOS PINHEIROS": "12:00",
                     "VILA PRADO": "12:05",
                     "VILA PAULO AYRES": "12:08",
-                    "VILA ARRUDA": "12:15",
-                    "RODOVIÁRIA": "12:25",
-                    "MERCADO MUNICIPAL": "12:35",
-                    "AV. CINCO DE NOVEMBRO": "12:45",
-                    "NASTRI II (BANCÁRIOS)": "12:47"
+                    "VILA ARRUDA": "12:09",
+                    "RODOVIÁRIA": "12:15",
+                    "MERCADO MUNICIPAL": "12:25",
+                    "AV. CINCO DE NOVEMBRO": "12:35",
+                    "NASTRI II (BANCÁRIOS)": "12:45"
                 }
             },
             "17:00": {
@@ -227,11 +227,11 @@ const Linha05B = {
                     "PORTAL DOS PINHEIROS": "17:00",
                     "VILA PRADO": "17:05",
                     "VILA PAULO AYRES": "17:08",
-                    "VILA ARRUDA": "17:15",
-                    "RODOVIÁRIA": "17:25",
-                    "MERCADO MUNICIPAL": "17:35",
-                    "AV. CINCO DE NOVEMBRO": "17:45",
-                    "NASTRI II (BANCÁRIOS)": "17:47"
+                    "VILA ARRUDA": "17:09",
+                    "RODOVIÁRIA": "17:15",
+                    "MERCADO MUNICIPAL": "17:25",
+                    "AV. CINCO DE NOVEMBRO": "17:35",
+                    "NASTRI II (BANCÁRIOS)": "17:45"
                 }
             }
         }
