c8019028f656b1c52b9f47ddef1be80a2e74a438
André Montanari <35246438+alphamontanari@users.noreply.github.com>
2026-03-31 17:09:20 -0300

    v.1.3.10.8
    atualização linha 11A

--- assets/linhas/linha11A.js
+++ assets/linhas/linha11A.js
@@ -86,16 +86,17 @@ const Linha11A = {
             }
         },
         "sabado": {
-            "06:00": {
+            "05:50": {
                 "atendimento": {
-                    "DISTRITO DO GRAMADINHO": "06:00",
-                    "DISTRITO DA CONCEIÇÃO": "06:10",
-                    "CASTROLANDA - COOPERATIVA AGROINDUSTRIA": "06:20",
-                    "PASSARELA DOMENICO ALCIATI (JD. BELA VISTA)": "06:22",
-                    "AV. 5 DE NOVEMBRO (VILA NASTRI I)": "06:25",
-                    "MERCADO MUNICIPAL": "06:35",
-                    "RODOVIÁRIA": "06:45"
-                }, servico: 'Distrito → Centro'
+                    "DISTRITO DO GRAMADINHO": "05:50",
+                    "DISTRITO DA CONCEIÇÃO": "06:00",
+                    "CASTROLANDA - COOPERATIVA AGROINDUSTRIA": "06:10",
+                    "PASSARELA DOMENICO ALCIATI (JD. BELA VISTA)": "06:12",
+                    "AV. 5 DE NOVEMBRO (VILA NASTRI I)": "06:15",
+                    "MERCADO MUNICIPAL": "06:25",
+                    "RODOVIÁRIA": "06:35"
+                },
+                "servico": "Distrito → Centro"
             },
             "08:00": {
                 "atendimento": {
